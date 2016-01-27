c4d6bf69941bcc0c7db7774c9eeda49507ffda02
Karan Barsiwal <Karan13047@iiitd.ac.in>
2016-01-27 15:58:25 +0530

    data added

--- app/src/main/java/in/ac/iiitd/iiitdapp/LoadData.java
+++ app/src/main/java/in/ac/iiitd/iiitdapp/LoadData.java
@@ -10,9 +10,46 @@ import in.ac.iiitd.iiitdapp.pojo.instructor;
  */
 public class LoadData {
     public List<instructor> data;
-    public LoadData(){
-        data  = new ArrayList<instructor>();
-        data.add(new instructor("https://www.iiitd.ac.in/sites/default/files/images/faculty/new/alex.jpg","Alexander Fell","Assistant Professor (ECE) alex@iiitd.ac.in  PhD (2012), Indian Institute of Science, Bangalore, India"));
 
+    public LoadData() {
+        data = new ArrayList<instructor>();
+        data.add(new instructor("https://www.iiitd.ac.in/sites/default/files/images/faculty/new/alex.jpg", "Alexander Fell", "Assistant Professor (ECE) alex@iiitd.ac.in  PhD (2012), Indian Institute of Science, Bangalore, India"));
+        data.add(new instructor("https://www.iiitd.ac.in/sites/default/files/images/faculty/new/anand.jpg", "Anand Srivastava", "Professor (ECE) anand@iiitd.ac.in  PhD (2003), Indian Institute of Technology Delhi"));
+        data.add(new instructor("https://www.iiitd.ac.in/sites/default/files/images/faculty/amarjeet.jpg", "Amarjeet Singh", "(Presently on leave)  Assistant Professor (CSE, ECE) amarjeet@iiitd.ac.in"));
+        data.add(new instructor("https://www.iiitd.ac.in/sites/default/files/images/faculty/new/angshul.jpg", "Angshul Majumdar", "Assistant Professor (ECE) angshul@iiitd.ac.in  PhD (2012), Electrical & Computer Engg., University of British Columbia, Canada"));
+        data.add(new instructor("https://www.iiitd.ac.in/sites/default/files/images/faculty/new/anubhag.jpg", "Anubha Gupta", "Associate Professor (ECE) anubha@iiitd.ac.in  Ph.D. (2006), Indian Institute of Technology-Delhi"));
+        data.add(new instructor("https://www.iiitd.ac.in/sites/default/files/images/faculty/new/apala.jpg", "Apala Guha", "(Presently on leave)  Assistant Professor (CSE) apala@iiitd.ac.in  PhD (2010), Computer Engineering, University of Virginia, Charlottesville, USA"));
+        data.add(new instructor("https://www.iiitd.ac.in/sites/default/files/images/faculty/new/akschand-forweb.jpg", "Arya Kumar Srustidhar Chand", "akschand@iiitd.ac.in  Ph.D from Ca' Foscari University of Venice, Italy (2013)  Specialization: Game theory, Industrial Organization, Finance"));
+        data.add(new instructor("https://www.iiitd.ac.in/sites/default/files/images/faculty/new/subramanyam.jpg", "A. V. Subramanyam", "Assistant Professor (CSE, ECE) subramanyam@iiitd.ac.in  PhD(2012), Computer Engineering, Nanyang Technological University, Singapore"));
+        data.add(new instructor("https://www.iiitd.ac.in/sites/default/files/images/faculty/chetana.jpg", "Chetan Arora", "Assistant Professor (CSE) chetan@iiitd.ac.in  PhD (2012), Indian Institute of Technology, Delhi"));
+        data.add(new instructor("https://www.iiitd.ac.in/sites/default/files/images/faculty/new/dbera.jpg", "Debajyoti Bera", "Assistant Professor (CSE) dbera@iiitd.ac.in  PhD (2009), Computer Science, Boston University, USA"));
+        data.add(new instructor("https://www.iiitd.ac.in/sites/default/files/images/faculty/donghoon.jpg", "Donghoon Chang", "Assistant Professor (CSE) donghoon@iiitd.ac.in  PhD (2008), Information Management and Security, Korea University, Korea"));
+        data.add(new instructor("https://www.iiitd.ac.in/sites/default/files/images/faculty/new/acharya.jpg", "Hrishikesh B. Acharya", "Assistant Professor (CSE) acharya@iiitd.ac.in  PhD (2012), Computer Science, University of Texas (at Austin)"));
+        data.add(new instructor("https://www.iiitd.ac.in/sites/default/files/images/faculty/new/mayank.jpg", "https://www.iiitd.ac.in/sites/default/files/images/faculty/new/mayank.jpg", "Associate Professor (CSE) mayank@iiitd.ac.in  PhD (2008), Computer Science, West Virginia University, USA"));
+        data.add(new instructor("https://www.iiitd.ac.in/sites/default/files/images/faculty/new/mshashmi.jpg", "Mohammad S. Hashmi", "Assistant Professor (ECE) mshashmi@iiitd.ac.in  PhD (2009), Electronics Engineering, Cardiff University, UK"));
+        data.add(new instructor("https://www.iiitd.ac.in/sites/default/files/images/faculty/new/ojaswa.jpg", "Ojaswa Sharma", "Assistant Professor (CSE) ojaswa@iiitd.ac.in  PhD (2010), Mathematics and Computer Science, Technical University of Denmark, Denmark"));
+        data.add(new instructor("https://www.iiitd.ac.in/sites/default/files/images/faculty/new/pbsujit.jpg", "P B Sujit", "Assistant Professor (ECE) sujit@iiitd.ac.in  PhD (2006), Aerospace Engineering, Indian Institute of Science, Bangalore"));
+        data.add(new instructor("https://www.iiitd.ac.in/sites/default/files/images/faculty/new/jalote.jpg", "Pankaj Jalote", "Director & Professor (CSE) jalote@iiitd.ac.in  PhD (1985), Computer Science, University of Illinois at Urbana Champaign, USA"));
+        data.add(new instructor("https://www.iiitd.ac.in/sites/default/files/images/faculty/new/pk.jpg", "Ponnurangam Kumaraguru", "Associate Professor (CSE) pk@iiitd.ac.in  PhD (2009), Computer Science, Carnegie Mellon University, USA"));
+        data.add(new instructor("https://www.iiitd.ac.in/sites/default/files/images/faculty/new/puneet-goyal.jpg", "Puneet Goyal", "Assistant Professor (CSE) pgoyal@iiitd.ac.in  PhD (2010), Purdue University, IN, USA"));
+        data.add(new instructor("https://www.iiitd.ac.in/sites/default/files/images/faculty/new/puspendra.jpg", "Pushpendra Singh", "Associate Professor (CSE) psingh@iiitd.ac.in  PhD (2004), Inria-Rennes, Université de Rennes 1, France"));
+        data.add(new instructor("https://www.iiitd.ac.in/sites/default/files/images/faculty/new/pravesh.jpg", "ravesh Biyani", "Assistant Professor praveshb@iiitd.ac.in  PhD (2012), ECE, IIT Delhi"));
+        data.add(new instructor("https://www.iiitd.ac.in/sites/default/files/images/faculty/new/richa.jpg", "Richa Singh", "Associate Professor (CSE) rsingh@iiitd.ac.in  PhD (2008), Computer Science, West Virginia University, USA"));
+        data.add(new instructor("https://www.iiitd.ac.in/sites/default/files/images/faculty/new/saket.jpg", "Saket Anand", "Assistant Professor (CSE, ECE) anands@iiitd.ac.in  PhD (2013), Electrical and Computer Engineering, Rutgers University, NJ, USA"));
+        data.add(new instructor("https://www.iiitd.ac.in/sites/default/files/images/faculty/new/sandip.jpg", "Sandip Aine", "(Presently on leave)  Assistant Professor (CSE) sandip@iiitd.ac.in  PhD, 2007 (Indian Institute of Technology, Kharagpur)"));
+        data.add(new instructor("https://www.iiitd.ac.in/sites/default/files/images/faculty/new/sanjit.jpg", "Sanjit Krishnan Kaul", "Assistant Professor (ECE) skkaul@iiitd.ac.in  PhD (2011), Electrical and Computer Engineering, Rutgers University, USA"));
+        data.add(new instructor("https://www.iiitd.ac.in/sites/default/files/images/faculty/new/sambuddho.jpg", "Sambuddho Chakravarty", "Assistant Professor (CSE) sambuddho@iiitd.ac.in  PhD (2014), Columbia University, USA"));
+        data.add(new instructor("https://www.iiitd.ac.in/sites/default/files/images/faculty/new/shobha.jpg", "Shobha Sundar Ram", "Assistant Professor (ECE) shobha@iiitd.ac.in  PhD (2009), Electrical Engineering, University of Texas at Austin"));
+        data.add(new instructor("https://www.iiitd.ac.in/sites/default/files/images/faculty/somitra.JPG", "Somitra Kr. Sanadhya", "Assistant Professor (CSE) somitra@iiitd.ac.in  PhD (2009), Computer Science, Indian Statistical Institute, Kolkata"));
+
+        data.add(new instructor("https://www.iiitd.ac.in/sites/default/files/images/faculty/new/sriram.jpg", "Sriram K", "Assistant Professor (COMP-BIO) sriramk@iiitd.ac.in  PhD (2004), Chemistry, IIT Madras"));
+        data.add(new instructor("https://www.iiitd.ac.in/sites/default/files/images/faculty/subhadip.jpg", "Subhadip Raychaudhuri", "Associate Professor (COMP-BIO) subhadip@iiitd.ac.in  Phd (2002), University of Rochester, USA"));
+
+        data.add(new instructor("https://www.iiitd.ac.in/sites/default/files/images/faculty/new/sujay.jpg", "Sujay Deb", "Assistant Professor (ECE) sdeb@iiitd.ac.in  PhD (2012), Washington State University"));
+        data.add(new instructor("https://www.iiitd.ac.in/sites/default/files/images/faculty/sumit.jpg", "Sumit J Darak", "Assistant Professor (ECE) sumit@iiitd.ac.in  PhD (2013), Nanyang Technological University (NTU), Singapore"));
+        data.add(new instructor("https://www.iiitd.ac.in/sites/default/files/images/faculty/new/viswanath.jpg", "Venkata M. Viswanath Gunturi", "Assistant Professor (CSE) gunturi@iiitd.ac.in  PhD (2015), Computer Science and Engineering, University of Minnesota, Twin Cities, USA"));
+        data.add(new instructor("https://www.iiitd.ac.in/sites/default/files/images/faculty/new/vikram.jpg", "Vikram Goyal", "Associate Professor (CSE) vikram@iiitd.ac.in  PhD (2009), Computer Science and Engineering, IIT Delhi"));
+        data.add(new instructor("https://www.iiitd.ac.in/sites/default/files/images/faculty/new/naik.jpg", "Vinayak Naik", "Associate Professor (CSE) naik@iiitd.ac.in  PhD (2006), Computer Science and Engineering, Ohio State University, USA"));
+        data.add(new instructor("https://www.iiitd.ac.in/sites/default/files/images/faculty/new/vivek.jpg", "Vivek Bohara", "Assistant Professor (ECE) vivek.b@iiitd.ac.in  PhD(2011), Electrical and Electronic Engineering, Nanyang Technological University, Singapore"));
     }
 }
